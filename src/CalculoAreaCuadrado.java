036f47ce4863dfd1bfb309b38fb50686f7d02f8f
Ximo <ximomingarro@hotmail.com>
2023-10-13 18:52:42 +0200

    Programita de Java area cuadrado

---
+++ src/CalculoAreaCuadrado.java
@@ -0,0 +1,12 @@
+public class CalculoAreaCuadrado {
+    public static void main(String[] args)
+    {
+        //Definimos la longitud del lado del cuadrado
+        double lado = 5.0;
+        //Calculamos el area del cuadrado
+        double area = lado * lado;
+
+        //Mostramos el resultado
+        System.out.println("El area del cuadrado con lado de longitud" + lado +" es: " + area);
+    }
+}
